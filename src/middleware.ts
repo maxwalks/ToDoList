a6ddd8f6243ec91e79386a60b718a5c98ac62a2c
maxwalks <78441835+maxwalks@users.noreply.github.com>
2025-02-13 14:57:46 +0100

    Bug fix

--- src/middleware.ts
+++ src/middleware.ts
@@ -3,20 +3,22 @@ import { NextResponse } from "next/server";
 import type { NextRequest } from "next/server";
 
 export async function middleware(request: NextRequest) {
-  const token = await getToken({ req: request, secret: process.env.NEXTAUTH_SECRET });
-  //const isAuthPage = request.nextUrl.pathname === "/register" || request.nextUrl.pathname === "/login";
+  const token = await getToken({ 
+    req: request, 
+    secret: process.env.NEXTAUTH_SECRET,
+    secureCookie: process.env.NODE_ENV === "production",
+  });
+
+  console.log("Middleware token:", token);
+  console.log("Current URL:", request.nextUrl.pathname);
 
   if (!token && request.nextUrl.pathname === "/") {
     return NextResponse.redirect(new URL("/register", request.url));
   }
 
-  //if (token && isAuthPage) {
-  //  return NextResponse.redirect(new URL("/", request.url));
-  //}
-
   return NextResponse.next();
 }
 
 export const config = {
-  matcher: ["/"] // "/register", "/login"
+  matcher: ["/"]
 };
